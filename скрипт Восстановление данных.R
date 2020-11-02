ce7518862576356d80ec586947e7744184268ffe
OSlavuta713 <boderad@ukr.net>
2020-11-02 18:13:44 +0200

    получение данніх для расчета MAPE

--- скрипт Восстановление данных.R
+++ скрипт Восстановление данных.R
@@ -3,6 +3,8 @@ library(readxl)
 # подключение библиотеки восстановения данных
 library(imputeTS)
 rwater <- read_excel("F:/My computer/Мои документы/ХНУ Каразина/Туманов А/rwater.xls")
+rwater <- read_excel("F:/My computer/Мои документы/ХНУ Каразина/Туманов А/rwater1.xls")
+
 View(rwater)
 plot(rwater)
 # восстановление данных
@@ -34,19 +36,52 @@ TBATS <- forecast(tbats(tsimportsplit, biasadj=TRUE), h=12)
 plot(TBATS)
 Combination <-(ETS[["mean"]] + ARIMA[["mean"]] + STL[["mean"]] +
 +                    NNAR[["mean"]] + TBATS[["mean"]])/5
+# подключение библиотеки чтения Екселя
+library(readxl)
+# подключение библиотеки восстановения данных
+library(imputeTS)
+rwater <- read_excel("F:/My computer/Мои документы/ХНУ Каразина/Туманов А/rwater.xls")
+View(rwater)
+plot(rwater)
+# восстановление данных
+tsen <- ts(rwater[,3], start=2010, frequency = 12)
+plot(tsen)
+tsimportmid <- na_mean (tsen, option = "mean", maxgap = Inf)
+plot(tsimportmid)
+tsimportsplit<- na_seasplit( tsen, algorithm = "interpolation", find_frequency = FALSE, maxgap = Inf )
+plot(tsimportsplit)
+tsimportrand <- na_random(tsen, lower_bound = NULL, upper_bound = NULL, maxgap = Inf)
+plot(tsimportrand)
+taimportremov <- na_remove(tsen)
+plot(taimportremov)
+tsimportrepl <- na_replace(tsen, fill = 0, maxgap = Inf)
+plot(tsimportrepl)
+# декомпозиция ряда
+st1 <- stl(tsimportsplit, t.window=13, s.window = 12)
+plot(st1)
+# прогнозирование разными методами 
+ETS <- forecast(ets(tsimportsplit), h=12)
+plot(ETS)
+STL <-stlf(tsimportsplit, lambda=0, h=12, biasadj=TRUE)
+plot(STL)
+ARIMA <- forecast(auto.arima(tsimportsplit, lambda=0, biasadj=TRUE),h=12)
+plot(ARIMA)
+NNAR <- forecast(nnetar(tsimportsplit), h=12)
+plot(NNAR)
+TBATS <- forecast(tbats(tsimportsplit, biasadj=TRUE), h=12)
+plot(TBATS)
+Combination <-(ETS[["mean"]] + ARIMA[["mean"]] + STL[["mean"]] +
+                 +                    NNAR[["mean"]] + TBATS[["mean"]])/5
 autoplot(tsimportsplit, linetype = "dashed") +
-+     autolayer(ETS, series="ETS", PI=FALSE) +
-+     autolayer(ARIMA, series="ARIMA", PI=FALSE) +
-+     autolayer(STL, series="STL", PI=FALSE) +
-+     autolayer(NNAR, series="NNAR", PI=FALSE) +
-+     autolayer(TBATS, series="TBATS", PI=FALSE) +
-+     autolayer(Combination, series="Combination") +
-+     xlab("Рік") 
-+     ylab("Імпорт води Україною")
-guides(colour=guide_legend(title=""))
+  +     autolayer(ETS, series="ETS", PI=FALSE) +
+  +     autolayer(ARIMA, series="ARIMA", PI=FALSE) +
+  +     autolayer(STL, series="STL", PI=FALSE) +
+  +     autolayer(NNAR, series="NNAR", PI=FALSE) +
+  +     autolayer(TBATS, series="TBATS", PI=FALSE) +
+  +     autolayer(Combination, series="Combination") +
+  +     xlab("Рік") +
++     ylab("Імпорт води Україною") +
+guides(colour=guide_legend(title="")) 
 scale_x_continuous(expand = c(0, 0))
 scale_y_continuous(expand = c(0, 0))
-scale_x_continuous(breaks= seq(2010, 2024, 2))
-
-
-
+scale_x_continuous(breaks= seq(2010, 2018, 2))
